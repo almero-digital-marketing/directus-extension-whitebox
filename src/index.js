602321fd2ec1852c375f5352102c4fa017ef3bb1
dimitarkolev <dk@almero.bg>
2022-07-01 06:02:50 +0000

    Removed change tracking

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { async } from 'hasha'
+
 const { machineIdSync } = require('node-machine-id')
 const aguid = require('aguid')
 const os = require('os')
@@ -11,35 +13,24 @@ const hasha = require('hasha')
 const path = require('path')
 const glob = require('glob-promise')
 const fs = require('fs-extra-promise')
-const { throttle, debounce } = require('throttle-debounce')
+const { debounce } = require('throttle-debounce')
 const yaml = require('js-yaml');
 const _ = require('lodash')
 require('deepdash')(_)
 
 export default async ({ filter, action, init }, options) => {
-	const stamp = Date.now()
+	let stamp = Date.now()
 	const machineId = machineIdSync() + '_' + os.hostname() + '_' + os.userInfo().username
 	let queue = Queue({
 		concurrency: 3,
 		autostart: true
 	})
 	const uploadsFolder = path.resolve(options.env['STORAGE_LOCAL_ROOT'])
-	const changes = new Set()
 	const itemDepth = [new Array(4).fill('*').join('.')]
 	const runtimeFolder = path.join(options.env['INIT_CWD'], 'runtime')
 	await fs.emptyDir(runtimeFolder)
 
 	const { ItemsService } = options.services;
-	const schema = await options.getSchema()
-	let { collections } = schema
-	const documentCollections = Object
-	.keys(collections)
-	.filter(key => key.indexOf('directus_') != 0)
-	.map(key => collections[key])
-	.filter(collection => {
-		return collection.fields.target && collection.fields.target.defaultValue == 'whitebox'
-	})
-
 	// console.log(JSON.stringify(schema, null, 2))
 	
 	let clear = Promise.resolve()
@@ -167,53 +158,7 @@ export default async ({ filter, action, init }, options) => {
 		return data
 	}
 
-	async function dumpDocument(data) {
-		const document = yaml.dump(data.data.meta)
-		const documentFile = path.join(runtimeFolder, data.refId + '.yml')
-		await fs.outputFileAsync(documentFile, document, 'utf8')
-		return documentFile
-	}
-
-	let importDocumentTimeouts = {}
-	async function importDocument({key, collection}, context) {
-		const timeoutId = `/${collection}/${key}`
-		clearTimeout(importDocumentTimeouts[timeoutId])
-		importDocumentTimeouts[timeoutId] = setTimeout(async () => {
-			const itemsService = new ItemsService(collection, {
-				schema: context.schema,
-			})
-	
-			let item = await itemsService.readOne(key, { 
-				fields: itemDepth,
-			})
-	
-			if (item.target != 'whitebox' || !item.layout) return
-			if (!options.env['WHITEBOX_CLEAR']) {
-				const data = dataItem(collection, item)
-	
-				const document = await dumpDocument(data)
-	
-				queue.push(() => {
-					console.log('Document imported:', document)
-					return api('feed', '/api/catalog/keep/one', data)
-				})
-			}
-		}, 1000)
-
-	}
-	
-	function deleteDocument({key, collection}, context) {
-		const refId = '/' + collection + '/' + key
-		const vaultId = aguid(refId)
-		if (!options.env['WHITEBOX_CLEAR']) {
-			queue.push(() => {
-				console.log('Document deleted:', refId)
-				return api('feed', '/api/catalog/remove', { vaultId })
-			})
-		}
-	}
-
-	async function sync() {
+	async function syncFilesAsync() {
 		if (!options.clear) {
 			let files = await glob('**/*', { cwd: uploadsFolder })
 			for (let file of files) {
@@ -225,159 +170,86 @@ export default async ({ filter, action, init }, options) => {
 			}
 		}
 	}
+	const syncFiles = debounce(1000, syncFilesAsync)
 
-	async function followRelations({key, collection}, context) {
-		const id = `/${collection}/${key}`
-		if (!key || changes.has(id)) return
-		changes.add(id)
-
-		const related = context.schema.relations.filter(relation => relation.collection == collection)
-		for (let relation of related) {
-			const itemsService = new ItemsService(collection, {
-				schema: context.schema,
-			})
-			
-			const item = await itemsService.readOne(key, { fields: ['*'] })
-			if (relation.related_collection != null) {
-				await changeRelatedItems({ key: item[relation.field], collection: relation.related_collection }, context)
-			} 
-			if (relation.meta.one_allowed_collections != null) {
-				for(let oneCollection of relation.meta.one_allowed_collections) {
-					await changeRelatedItems({ key: item[relation.field], collection: oneCollection }, context)
-				}
-			}
-		}
-	}
-
-	async function changeRelatedItems({key, collection}, context) {
-		const id = `/${collection}/${key}`
-		if (!key || changes.has(id)) return
-		changes.add(id)
-
-		const related = context.schema.relations
-		.filter(relation => (relation.related_collection && relation.related_collection == collection) || (relation.meta.one_allowed_collections && relation.meta.one_allowed_collections.indexOf(collection) > -1))
-
-        for (let relation of related) {
-			const itemsService = new ItemsService(relation.collection, {
-				schema: context.schema,
-			})
-			if (context.schema.collections[relation.collection].fields[relation.field].type == 'integer') {
-				key = Number(key)
-			}
-			const filter = { [relation.field]: key }
-            const items = await itemsService.readByQuery({ filter })
-			for (let item of items) {
-				await followRelations({ key: item.id, collection: relation.collection }, context)
-			}
-        }
-    }
-
-	async function processChanges(context) {
-		if (changes.size > 1) {
-			let ids = [...changes].slice(1)
-			console.log('Changes:', ids.join(', '))
-			for (let id of ids) {
-				const [,relatedCollection, relatedKey] = id.split('/')
-				await importDocument({ key: relatedKey, collection: relatedCollection }, context)
-			}
-		}
-		changes.clear()
-	}
-
-	action('items.create', async (meta, context) => {
-		console.log('Items create action')
-		try {
-			await importDocument(meta, context)
-		} catch (e) {
-			console.error('Items create action failed', meta, e)
-		}
-	})
-	filter('items.update', async (payload, meta, context) => {
-		try {
-			for(let key of meta.keys) {
-				console.log('Items update filter:', meta.collection, key)
-				await changeRelatedItems({key, collection: meta.collection}, context)
-			}
-		} catch (e) {
-			console.error('Items update filter failed', e)
-		}
-	})
-	action('items.update', async (meta, context) => {
-		try {
-			for(let key of meta.keys) {
-				console.log('Items update action:', meta.collection, key)
-				await importDocument({key, collection: meta.collection}, context)
-				await changeRelatedItems({key, collection: meta.collection}, context)
-			}
-
-			await processChanges(context)
-		} catch (e) {
-			console.error('Items update action failed', meta, e)
-		}
-	})
-	filter('items.delete', async (keys, { collection }, context) => {
-		try {
-			for(let key of keys) {
-				console.log('Items delete filter:', collection, key)
-				await changeRelatedItems({key, collection, changes}, context)
-			}
-			await processChanges(context)
-		} catch (e) {
-			console.error('Items delete filter failed', e)
-		}
-	})
-	action('items.delete', async (meta, context) => {
-		try {
-			for(let key of meta.payload) {
-				console.log('Items delete action:', meta.collection, key)
-				await deleteDocument({key, collection: meta.collection}, context)
-			}
-			await processChanges(context)
-		} catch (e) {
-			console.error('Items delete action failed', meta, e)
-		}
-	})
-	action('files.upload', sync)
+	let documentsSyncStatus = {}
+	let layouts = []
+	async function syncDocumentsAsync() {
+		const schema = await options.getSchema()
+		let { collections } = schema
+		const documentCollections = Object
+		.keys(collections)
+		.filter(key => key.indexOf('directus_') != 0)
+		.map(key => collections[key])
+		.filter(collection => {
+			return collection.fields.target && collection.fields.target.defaultValue == 'whitebox'
+		})
 	
-	init('app.after', async () => {
 		console.log('Document collections:', documentCollections.length)
 		for(let documentCollection of documentCollections) {
 			const itemsService = new ItemsService(documentCollection.collection, {
 				schema,
 			})
-
+	
 			const items = await itemsService.readByQuery({ 
 				fields: itemDepth 
 			})
-			const layouts = []
 			console.log(`Collection [${documentCollection.collection}]:`, items.length)
 			for(let item of items) {
 				if (item.target != 'whitebox' || !item.layout) continue
 				if (layouts.indexOf(item.layout) == -1) layouts.push(item.layout)
+	
 				if (!options.env['WHITEBOX_CLEAR']) {
 					const data = dataItem(documentCollection.collection, item)
-
-					const document = await dumpDocument(data)
-
-					queue.push(() => {
-						console.log('Document imported:', document)
-						return api('feed', '/api/catalog/keep/one', data)
-					})
+	
+					let checksum = await hasha.async(JSON.stringify(data.data.meta))
+					if (documentsSyncStatus[data.refId] != checksum) {
+						documentsSyncStatus[data.refId] = checksum
+	
+						const document = yaml.dump(data.data.meta)
+						const documentFile = path.join(runtimeFolder, data.refId + '.yml')
+						await fs.outputFileAsync(documentFile, document, 'utf8')
+				
+						queue.push(() => {
+							console.log('Import document:', documentFile)
+							return api('feed', '/api/catalog/keep/one', data)
+						})
+					}
+	
 				}
 			}
-			
+		}
+	}
+	const syncDocuments = debounce(1000, syncDocumentsAsync)
+
+	async function expireDocumentsAsync() {
+		documentsSyncStatus = {}
+		stamp = Date.now()
+		layouts = []
+	
+		await syncDocumentsAsync()
+	
+		queue.push(async () => {
 			console.log('Layouts:', layouts.join(', '))
 			for(let layout of layouts) {
-				queue.push(() => {
-					return api('feed', '/api/catalog/expire', {
-						type: 'directus.' + layout,
-						stamp
-					})
-				}) 
+				console.log('Expire layout:', layout)
+				await api('feed', '/api/catalog/expire', {
+					type: 'directus.' + layout,
+					stamp
+				})
 			}
-		}
-		await sync()
-		console.log('Application started!');
+		})
+	}
+	const expireDocuments = debounce(1000, expireDocumentsAsync)
+
+	action('items.create', syncDocuments)
+	action('items.update', syncDocuments)
+	action('items.delete', expireDocuments)
+
+	init('app.after', async () => {
+		expireDocuments()
+		syncFiles()
+		console.log('WhiteBox sync started!');
 	})
 
 	queue.on('success', debounce(1000, () => {
